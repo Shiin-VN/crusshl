a8ba5108b7df28161b0f2a3ac6e76c8a2608456b
Shiin-VN <nguyenvanquyet.id2021@gmail.com>
2024-08-27 20:52:35 +0700

    Update myJs.js

--- js/myJs.js
+++ js/myJs.js
@@ -1,16 +1,16 @@
 const textConfig = {
-  text1: "Helo cậu!",
-  text2: "Tớ Muốn Hỏi Cậu Một Câu Hỏi <3",
-  text3: "Cậu Có Thích Tớ Không ._.",
-  text4: "Nếu cậu ko trả lời mà thoát ra tức là muốn làm vợ tớ rùi đó nha :v",
-  text5: "Cậu mơ à???",
-  text6: "Tất Nhiên Là Có rùiii<3",
-  text7: "Lí do cậu thích tớ đi :vvvv",
-  text8: "Gửi cho tớ <3",
-  text9: "Vì tớ thích cậu mất rồi",
-  text10: "Tớ biết mà ^^ Yêu cậu 300.000",
+  text1: "Hello tngan !",
+  text2: "Tui muốn hỏi tngan 1 câu hỏi",
+  text3: "Tngan có ghét tui hong ._.",
+  text4: "Nếu TNgan thoát ra mà k trl là TNgan có ghét tui đó:(",
+  text5: "Tất nhiên là hong roài:))",
+  text6: "Có, siu siu ghét",
+  text7: "Lí do TNgan ghét tui :vvvv",
+  text8: "Gửi cho tui <3",
+  text9: "Vì TNgan bị VQ chọc quài nên ghét",
+  text10: "Thấy chưa biết ngay mà:(((",
   text11:
-    "Tối nay tớ qua đón cậu đi chơi nhaa :v Còn giờ thì cậu xem hết bí mật này nha!",
+    "Vậy TNgan cho tui xin lỗi nha, mốt tui hong chọc TNgan nua🥹",
   text12: "Okii lunn <3",
 };
 
